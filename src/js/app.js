28b75d53ca1b069a0d37be048f071235491b39cf
sashimii <stailor@metrolatorsmbp3.torstar.net>
2017-04-18 18:13:51 -0400

    Started the QGS redesign

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,15 @@
-var title = require('./title');
-var something = 'something';
+var typed = require('./typed');
+var leaflet = require('leaflet');
 
-title();
+typed();
+
+
+var mymap = leaflet.map('hero-map', {
+  attributionControl: false,
+  zoomControl: false,
+}).setView([51.505, -0.09], 13);
+
+leaflet.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2FzaGltaWkiLCJhIjoiY2llc3pleTQ3MDBrcHFhbTNjbmRnZjdpbyJ9.DGX7v39rkSvNnH9SrOvUaw', {
+    attribution: '',
+    maxZoom: 18,
+}).addTo(mymap);
